b765e8478724c2d69f1b74375b1e2c6d8982bf06
yush <yush2202@gmail.com>
2025-10-19 18:21:38 +0900

    refactor: 트랜잭션 서비스 추상화

---
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -0,0 +1,65 @@
+package com.techcourse.service;
+
+import com.interface21.dao.DataAccessException;
+import com.interface21.jdbc.datasource.DataSourceUtils;
+import com.interface21.transaction.support.TransactionSynchronizationManager;
+import com.techcourse.domain.User;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TxUserService implements UserService {
+
+    private static final Logger log = LoggerFactory.getLogger(TxUserService.class);
+
+    private final UserService userService;
+    private final DataSource dataSource;
+
+    public TxUserService(final UserService userService, final DataSource dataSource) {
+        this.userService = userService;
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public User findById(final long id) {
+        return userService.findById(id);
+    }
+
+    @Override
+    public void save(final User user) {
+        userService.save(user);
+    }
+
+    @Override
+    public void changePassword(final long id, final String newPassword, final String createdBy) {
+        Connection connection = null;
+        try {
+            connection = DataSourceUtils.getConnection(dataSource);
+            connection.setAutoCommit(false);
+            try {
+                userService.changePassword(id, newPassword, createdBy);
+                connection.commit();
+            } catch (SQLException exceptionWhenCommit) {
+                log.error("커밋 실패", exceptionWhenCommit);
+
+                try {
+                    connection.rollback();
+                    throw new DataAccessException("커밋 중 에러가 발생했습니다. 롤백합니다.", exceptionWhenCommit);
+                } catch (SQLException exceptionWhenRollback) {
+                    log.error("롤백 실패", exceptionWhenRollback);
+                    throw new DataAccessException("롤백 중 에러가 발생했습니다.", exceptionWhenRollback);
+                }
+            }
+        } catch (SQLException exceptionWhenConnect) {
+            log.error("DB 커넥션 획득 실패.", exceptionWhenConnect);
+            throw new DataAccessException("커넥션을 얻지 못했습니다.", exceptionWhenConnect);
+        } finally {
+            if (connection != null) {
+                DataSourceUtils.releaseConnection(connection, dataSource);
+                TransactionSynchronizationManager.unbindResource(dataSource);
+            }
+        }
+    }
+}
--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,77 +1,9 @@
 package com.techcourse.service;
 
-import com.interface21.dao.DataAccessException;
-import com.interface21.jdbc.datasource.DataSourceUtils;
-import com.interface21.transaction.support.TransactionSynchronizationManager;
-import com.techcourse.dao.UserDao;
-import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
-import com.techcourse.domain.UserHistory;
-import java.sql.Connection;
-import java.sql.SQLException;
-import javax.sql.DataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class UserService {
-
-    private static final Logger log = LoggerFactory.getLogger(UserService.class);
-
-
-    private final UserDao userDao;
-    private final UserHistoryDao userHistoryDao;
-    private final DataSource dataSource;
-
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao, final DataSource dataSource) {
-        this.userDao = userDao;
-        this.userHistoryDao = userHistoryDao;
-        this.dataSource = dataSource;
-    }
-
-    public User findById(final long id) {
-        return userDao.findById(id)
-                .orElseThrow(IllegalArgumentException::new);
-    }
-
-    public void insert(final User user) {
-        userDao.insert(user);
-    }
-
-    public void changePassword(final long id, final String newPassword, final String createBy) {
-        Connection connection = null;
-        try { // 외부 try - catch: connection 연결 과정 에러 관리
-            connection = DataSourceUtils.getConnection(dataSource);
-            connection.setAutoCommit(false);
-
-            try { // 내부 try - catch 트랜잭션 관리 보장.
-                final var user = findById(id);
-                user.changePassword(newPassword);
-                userDao.update(user);
-                userHistoryDao.log(new UserHistory(user, createBy));
-
-                connection.commit();
-            } catch (SQLException exceptionWhenCommit) {
-
-                try { // 롤백 try - catch
-                connection.rollback();
-                } catch (SQLException exceptionWhenRollback) {
-                    log.error("트랜잭션 롤벡 실패", exceptionWhenRollback);
-                    throw new DataAccessException("롤백 중 에러가 발생했습니다.", exceptionWhenRollback);
-                }
-
-                log.error("커밋 실패", exceptionWhenCommit);
-                throw new DataAccessException("커밋 중 에러가 발생했습니다. 롤백합니다.", exceptionWhenCommit);
-            }
-
-        } catch (SQLException exceptionWhenConnect) {
-            log.error("DB 커넥션 획득 실패.", exceptionWhenConnect);
-            throw new DataAccessException("커넥션을 얻지 못했습니다.", exceptionWhenConnect);
-
-        } finally {
-            if (connection != null) {
-                DataSourceUtils.releaseConnection(connection, dataSource);
-                TransactionSynchronizationManager.unbindResource(dataSource);
-            }
-        }
-    }
+public interface UserService {
+    User findById(final long id);
+    void save(final User user);
+    void changePassword(final long id, final String newPassword, final String createdBy);
 }
